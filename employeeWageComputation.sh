7762aad4d3bed5e7264ecc9172521dd8dc432534
parmeshwarraut1996 <45873296+parmeshwarraut1996@users.noreply.github.com>
2022-03-03 09:34:38 +0530

    REFACTORED:employee is absent then calculated salary

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -3,15 +3,17 @@
 #CONSTANTS
 IS_PRESENT=1;
 EMP_RATE_PER_HR=20
-EMP_HRS=8
 
 checkEmp=$((RANDOM%2))
 
 if [ $checkEmp -eq $IS_PRESENT ]
 then
+	EMP_HRS=8
 	echo "employee is present"
 	salary=$(($EMP_RATE_PER_HR*$EMP_HRS))
 	echo "Salary=$salary"
 else
+	EMP_HRS=0
 	echo "employee is absent"
+	echo "salary=0"
 fi
